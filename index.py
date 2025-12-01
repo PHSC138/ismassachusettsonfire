a1f9520780fab6badae6d820a4eae634a0ee7d4f
PHSC138 <PHSC138@users.noreply.github.com>
2025-11-30 23:20:28 -0500

    Updated indicies

--- index.py
+++ index.py
@@ -11,37 +11,123 @@ import time
 #         42.3601, -71.0589
 # Got the most coverage from
 #     https://www.freemaptools.com/radius-around-point.htm
+# Yes there's a ton of overlap and some overrun into neighboring states, but it's better than not having parts of mass included.
 mass = [
-        {
-            "lat": "42.437",
-            "lng": "-71.163",
-            "rad": "30",
-        },
-        {
-            "lat": "42.400",
-            "lng": "-71.669",
-            "rad": "25",
-        },
-        {
-            "lat": "42.355",
-            "lng": "-72.191",
-            "rad": "30",
-        },
-        {
-            "lat": "42.399",
-            "lng": "-72.707",
-            "rad": "30",
-        },
-        {
-            "lat": "42.391",
-            "lng": "-73.240",
-            "rad": "30",
-        },
-        {
-            "lat": "41.874",
-            "lng": "-70.556",
-            "rad": "35",
-        }
+    {
+        "lat": "42.553249"
+        "lng": "-73.118995"
+        "rad": "15",
+    },
+    {
+        "lat": "42.530989"
+        "lng": "-72.608240"
+        "rad": "15",
+    },
+    {
+        "lat": "42.255120"
+        "lng": "-71.435701"
+        "rad": "16",
+    },
+    {
+        "lat": "41.693766"
+        "lng": "-70.389352"
+        "rad": "16",
+    },
+    {
+        "lat": "41.393638"
+        "lng": "-70.652968"
+        "rad": "16",
+    },
+    {
+        "lat": "42.259186"
+        "lng": "-73.149123"
+        "rad": "17",
+    },
+    {
+        "lat": "42.374947"
+        "lng": "-72.069947"
+        "rad": "25",
+    },
+    {
+        "lat": "41.975997"
+        "lng": "-70.913885"
+        "rad": "26",
+    },
+    {
+        "lat": "42.567410"
+        "lng": "-70.932948"
+        "rad": "22",
+    },
+    {
+        "lat": "42.238855"
+        "lng": "-72.726129"
+        "rad": "18",
+    },
+    {
+        "lat": "41.978039"
+        "lng": "-70.081668"
+        "rad": "13",
+    },
+    {
+        "lat": "41.677186"
+        "lng": "-69.925147"
+        "rad": "11",
+    },
+    {
+        "lat": "41.302743"
+        "lng": "-70.139334"
+        "rad": "11",
+    },
+    {
+        "lat": "41.592937"
+        "lng": "-70.945459"
+        "rad": "11",
+    },
+    {
+        "lat": "42.597745"
+        "lng": "-71.559080"
+        "rad": "10",
+    },
+    {
+        "lat": "42.591595"
+        "lng": "-71.357394"
+        "rad": "10",
+    },
+    {
+        "lat": "42.602715"
+        "lng": "-71.798126"
+        "rad": "10",
+    },
+    {
+        "lat": "42.095250"
+        "lng": "-71.564839"
+        "rad": "10",
+    },
+    {
+        "lat": "42.102065"
+        "lng": "-72.358660"
+        "rad": "9",
+    },
+    {
+        "lat": "42.137483"
+        "lng": "-73.409109"
+        "rad": "8",
+    },
+    {
+        "lat": "42.088117"
+        "lng": "-71.806486"
+        "rad": "8",
+    },
+    {
+        "lat": "42.668384"
+        "lng": "-72.368152"
+        "rad": "7",
+    },
+    {
+        "lat": "42.675452"
+        "lng": "-72.863847"
+        "rad": "7",
+    }
 ]
 
 
@@ -76,11 +162,12 @@ def do_response(html, status_code="200 OK"):
     print(html)
     print("\r\n")
 
+
 def do_error(code):
     do_response(f"<html><head><title>Is Massachusetts On Fire?</title></head><body><h1>Internal Server Error</h1>Error code: {code}</body></html>", "500 Internal Server Error")
 
 
-# DO the thingy here
+# entry
 # Get now time in seconds
 now = time.time()
 
